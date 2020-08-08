1cd67413789996e4ecb84639b363ee199b7025e8
Alberto Cortés <alcortesm@gmail.com>
2020-08-08 14:49:15 +0200

    3.3 improve tests

--- ch03/p03/p03_test.go
+++ ch03/p03/p03_test.go
@@ -14,68 +14,67 @@ func TestSetOfStacks(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	checkDump(t, s, [][]int{})
+	checkEmpty(t, s)
+	checkPop(t, s, 0, false, [][]int{})
 
-	s.Push(0)
-	checkDump(t, s, [][]int{{0}})
-	s.Push(1)
-	checkDump(t, s, [][]int{{0, 1}})
-	s.Push(2)
-	checkDump(t, s, [][]int{{0, 1, 2}})
+	checkPush(t, s, 0, [][]int{{0}})
+	checkPush(t, s, 1, [][]int{{0, 1}})
+	checkPush(t, s, 2, [][]int{{0, 1, 2}})
 
-	checkPop(t, s, 2)
-	checkDump(t, s, [][]int{{0, 1}})
-	checkPop(t, s, 1)
-	checkDump(t, s, [][]int{{0}})
-	checkPop(t, s, 0)
+	checkPop(t, s, 2, true, [][]int{{0, 1}})
+	checkPop(t, s, 1, true, [][]int{{0}})
+	checkPop(t, s, 0, true, [][]int{})
+	checkPop(t, s, 0, false, [][]int{})
 	checkEmpty(t, s)
 
-	s.Push(0)
-	s.Push(1)
-	s.Push(2)
-	checkDump(t, s, [][]int{{0, 1, 2}})
-	s.Push(3)
-	checkDump(t, s, [][]int{{0, 1, 2}, {3}})
-	s.Push(4)
-	s.Push(5)
-	s.Push(6)
-	s.Push(7)
-	checkDump(t, s, [][]int{{0, 1, 2}, {3, 4, 5}, {6, 7}})
+	checkPush(t, s, 0, [][]int{{0}})
+	checkPush(t, s, 1, [][]int{{0, 1}})
+	checkPush(t, s, 2, [][]int{{0, 1, 2}})
+	checkPush(t, s, 3, [][]int{{0, 1, 2}, {3}})
+	checkPush(t, s, 4, [][]int{{0, 1, 2}, {3, 4}})
+	checkPush(t, s, 5, [][]int{{0, 1, 2}, {3, 4, 5}})
+	checkPush(t, s, 6, [][]int{{0, 1, 2}, {3, 4, 5}, {6}})
+	checkPush(t, s, 7, [][]int{{0, 1, 2}, {3, 4, 5}, {6, 7}})
 
-	checkPop(t, s, 7)
-	checkPop(t, s, 6)
-	checkDump(t, s, [][]int{{0, 1, 2}, {3, 4, 5}})
-	checkPop(t, s, 5)
-	checkPop(t, s, 4)
-	checkDump(t, s, [][]int{{0, 1, 2}, {3}})
-	checkPop(t, s, 3)
-	checkPop(t, s, 2)
-	checkDump(t, s, [][]int{{0, 1}})
-	checkPop(t, s, 1)
-	checkPop(t, s, 0)
-	checkDump(t, s, [][]int{})
+	checkPop(t, s, 7, true, [][]int{{0, 1, 2}, {3, 4, 5}, {6}})
+	checkPop(t, s, 6, true, [][]int{{0, 1, 2}, {3, 4, 5}})
+	checkPop(t, s, 5, true, [][]int{{0, 1, 2}, {3, 4}})
+	checkPop(t, s, 4, true, [][]int{{0, 1, 2}, {3}})
+	checkPop(t, s, 3, true, [][]int{{0, 1, 2}})
+	checkPop(t, s, 2, true, [][]int{{0, 1}})
+	checkPop(t, s, 1, true, [][]int{{0}})
+	checkPop(t, s, 0, true, [][]int{})
+
+	checkPop(t, s, 0, false, [][]int{})
 	checkEmpty(t, s)
 }
 
-func checkDump(t *testing.T, s *p03.SetOfStacks, want [][]int) {
+func checkPush(t *testing.T, s *p03.SetOfStacks, v int, dump [][]int) {
 	t.Helper()
 
+	s.Push(v)
+
 	got := s.Dump()
-	if diff := cmp.Diff(want, got); diff != "" {
+	if diff := cmp.Diff(dump, got); diff != "" {
 		t.Errorf("(-want +got)\n%s", diff)
 	}
 }
 
-func checkPop(t *testing.T, s *p03.SetOfStacks, want int) {
+func checkPop(t *testing.T, s *p03.SetOfStacks, v int, ok bool, dump [][]int) {
 	t.Helper()
 
-	got, ok := s.Pop()
-	if !ok {
-		t.Fatal("empty")
+	gotValue, gotOK := s.Pop()
+	if ok != gotOK {
+		t.Errorf("wron OK: want %t got %t", ok, gotOK)
 	}
 
-	if want != got {
-		t.Errorf("want %d, got %d", want, got)
+	if v != gotValue {
+		t.Errorf("wrong value: want %d, got %d", v, gotValue)
+	}
+
+	got := s.Dump()
+	if diff := cmp.Diff(dump, got); diff != "" {
+		t.Errorf("(-want +got)\n%s", diff)
 	}
 }
 
